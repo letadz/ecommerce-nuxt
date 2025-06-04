b062ebfbcfe07df749313a12b6c74a4bea00b576
Mark Angelo <mletada@traxiontech.net>
2025-06-04 15:55:15 +0800

    - Remove unused code - Modify nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV == "dev" },
   // ssr: false,
   app: {
     head: {
@@ -19,6 +19,13 @@ export default defineNuxtConfig({
         { name: "keywords", content: "E-commerce" },
       ],
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      script: [
+        {
+          src: "https://accounts.google.com/gsi/client",
+          async: true,
+          defer: true,
+        },
+      ],
     },
   },
   css: [
@@ -27,42 +34,18 @@ export default defineNuxtConfig({
     "animate.css",
     "vue3-carousel/dist/carousel.css",
   ],
-  modules: ["shadcn-nuxt", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
   pinia: {
     storesDirs: ["./stores/**"],
   },
-  tailwindcss: {
-    config: {
-      content: [
-        "./components/**/*.{vue,js,ts}",
-        "./layouts/**/*.{vue,js,ts}",
-        "./pages/**/*.{vue,js,ts}",
-        "./app.vue",
-        "./plugins/**/*.{js,ts}",
-        "./nuxt.config.{js,ts}",
-      ],
-      theme: {
-        extend: {
-          colors: {
-            primary: "#6366f1",
-            secondary: "#f3f4f6",
-            destructive: "#ef4444",
-            ring: "#6366f1",
-            accent: "#f1f5f9",
-            background: "#ffffff",
-            input: "#d1d5db",
-          },
-        },
-      },
-      plugins: [],
-    },
-  },
+  components: true,
 
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
       supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
       supabaseKey: process.env.NUXT_PUBLIC_SUPABASE_KEY,
+      googleClientId: process.env.NUXT_PUBLIC_CLIENT_ID,
     },
   },
 });
